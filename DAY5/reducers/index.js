dd8fdc49b9753926f8910dc162a32712a8aea4d4
Vinothini15r <120247771+Vinothini15r@users.noreply.github.com>
2023-09-23 16:09:03 +0530

    Delete DAY5 directory

--- DAY5/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import movieReducer from './movieReducer';
-import './red.css';
-
-const rootReducer = combineReducers({
-  movies: movieReducer
-});
-
-export default rootReducer;
--- DAY5/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers';
-import './sto.css';
-
-const store = createStore(rootReducer);
-
-export default store;
