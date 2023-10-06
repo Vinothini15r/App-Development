a6dfb254a49be2dd84b3adb74eed0754792bc8c6
Vinothini15r <120247771+Vinothini15r@users.noreply.github.com>
2023-10-06 10:24:11 +0300

    Add files via upload

---
+++ Day14/src/main/java/com/ecommerce/service/ReviewService.java
@@ -0,0 +1,38 @@
+package com.ecommerce.service;
+
+
+
+        import com.ecommerce.entity.Review;
+        import com.ecommerce.repository.ReviewRepository;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
+
+        import java.util.List;
+        import java.util.Optional;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public List<Review> getAllReviews() {
+        return reviewRepository.findAll();
+    }
+
+    public Optional<Review> getReviewById(Long id) {
+        return reviewRepository.findById(id);
+    }
+
+    public Review saveReview(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public void deleteReview(Long id) {
+        reviewRepository.deleteById(id);
+    }
+}
