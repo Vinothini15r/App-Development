163f09b9d7cdbfcaa753c71a92a2f52e96d819b8
Vinothini15r <120247771+Vinothini15r@users.noreply.github.com>
2023-10-06 15:48:08 +0530

    Add files via upload

---
+++ Day13/src/components/Products.jsx
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addCart } from "../redux/action";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+import { Link } from "react-router-dom";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
+
+  const dispatch = useDispatch();
+
+  const addProduct = (product) => {
+    dispatch(addCart(product));
+  };
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/products" , {
+        headers: {
+            'Authorization':"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYWFAZ21haWwuY29tIiwiaWF0IjoxNjk2NTY5Njc1LCJleHAiOjE2OTY2NTYwNzV9.vj2Tx-oudMmFq6wGQJ5eC_VdBRWZuM_krLrffRGxH1E",
+            'Content-Type':"application/json"
+        }});
+        const responseData = await response.json();
+
+        if (componentMounted) {
+          if (Array.isArray(responseData)) {
+            setData(responseData);
+            setFilter(responseData);
+          } else {
+            console.error("Response data is not an array:", responseData);
+            setFilter([]);
+          }
+
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+
+      return () => {
+        componentMounted = false;
+      };
+    };
+
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-12 py-5 text-center">
+          <Skeleton height={40} width={560} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+      </>
+    );
+  };
+
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((item) => item.category === cat);
+    setFilter(updatedList);
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons text-center py-5">
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => setFilter(data)}>All</button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("dairy")}>Dairy</button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("vegetables")}>
+            Vegetables
+          </button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("fruits")}>Fruits</button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("meat")}>Meat</button>
+        </div>
+
+        {filter.map((product) => {
+          return (
+            <div id={product.id} key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+              <div className="card text-center h-100" key={product.id}>
+                <img
+                  className="card-img-top p-3"
+                  src={product.image}
+                  alt="Card"
+                  height={300}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {product.title.substring(0, 12)}...
+                  </h5>
+                  <p className="card-text">
+                    {product.description.substring(0, 90)}...
+                  </p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item lead">$ {product.price}</li>
+                </ul>
+                <div className="card-body">
+                  <Link to={"/product/" + product.id} className="btn btn-dark m-1">
+                    Buy Now
+                  </Link>
+                  <button className="btn btn-dark m-1" onClick={() => addProduct(product)}>
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className="container my-3 py-3">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="display-5 text-center">Latest Products</h2>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Products;
