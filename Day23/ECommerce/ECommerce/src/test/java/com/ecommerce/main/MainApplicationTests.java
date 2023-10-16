f213b8fd920e62abce1e8226d4270ece266da1ad
Vinothini15r <120247771+Vinothini15r@users.noreply.github.com>
2023-10-16 11:17:04 +0530

    Add files via upload

---
+++ Day23/ECommerce/ECommerce/src/test/java/com/ecommerce/main/MainApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ecommerce.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
